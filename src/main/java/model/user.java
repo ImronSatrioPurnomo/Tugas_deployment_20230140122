ff82a57f49f179b9ffee2231ee3b97c859f3b390
satri <satriogames2223@gmail.com>
2026-04-20 14:17:16 +0700

    membuat file user.java dan userRepository

---
+++ src/main/java/model/user.java
@@ -0,0 +1,25 @@
+package model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table (nama = "20230140122_Users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class user {
+    @Id
+    private String id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String name;
+}
