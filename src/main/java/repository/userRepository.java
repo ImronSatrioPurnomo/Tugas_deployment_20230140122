f422769ed7e3c6f09b18851cb4ab79e844cd689e
satri <satriogames2223@gmail.com>
2026-04-20 14:20:26 +0700

    menambahkan @repository di baris 8

--- src/main/java/repository/userRepository.java
+++ src/main/java/repository/userRepository.java
@@ -1,4 +1,10 @@
 package repository;
 
-public interface userRepository {
+import org.apache.catalina.User;
+import org.springframework.boot.webmvc.autoconfigure.WebMvcProperties;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface userRepository extends JpaRepository<User, String> {
 }
