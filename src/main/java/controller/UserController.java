23482149078a78b96291d587be79fba2fb984504
satri <satriogames2223@gmail.com>
2026-04-20 14:46:14 +0700

    membuat file UserController.java

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,4 +1,39 @@
 package controller;
 
+
+import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.*;
+import service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private final UserService userService;
+    public UserController(UserService userService){
+        this.userService = userService
+    }
+
+    @PostMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUser();
+    }
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable String id){
+        userService.deleteUser(id);
+        return "user deleted successfully";
+    }
+
+    @GetMapping("{/id}"){
+        public User getUserById(id);
+    }
+
+    @PutMapping("{/id}")
+    public String updateUser (@PathVariable String id, @RequestBody user request){
+        userService.updateuser(id, request);
+        return "user updated successfully";
+    }
+
+
 }
